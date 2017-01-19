622d2acec4e7f1d3c8de27c32bca6f537ccfd653
Gerrod Mozeik <gerrodmozeik@zipcoders-MacBook-Pro-9.local>
2017-01-19 17:40:38 -0500

    First loop

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ var display = document.getElementById("display");
 function yourFunctionName (){
   display.innerHTML = "hello";
 }
+
+function lab1 () {
+   for (int i = 1; i < 11; i++) {
+     document.write(i);
+   }
+}
